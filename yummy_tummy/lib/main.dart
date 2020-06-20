b093d3c42c0167bbb68637e9af4bb79f0b20664f
Michiel Proost <michiel.proost@gmail.com>
2020-06-20 17:04:00 +0200

    Tried To Add User with Firestore - Not working
    Also got some errors that I was missing files in my Flutter directory. Need to investigate

--- yummy_tummy/lib/main.dart
+++ yummy_tummy/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:yummytummy/app.dart';
+import 'package:yummytummy/model/Database.dart';
+import 'package:yummytummy/model/user.dart';
+
+/// For testing purposes. Is to be removed later.
+void testFirestore(){
+  print("TESTING");
+  Database db = new Database();
+  db.addUser('tempUser', 0, RankType.beginner);
+  print("TESTING");
+}
 
 /// Main function just runs the app.
 void main() {
+  testFirestore();
   runApp(new RecipesApp());
 }
