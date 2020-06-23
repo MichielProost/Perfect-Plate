93b54e066340391d0a4dc82035918a588f33e1dc
Michiel Proost <michiel.proost@gmail.com>
2020-06-23 17:02:59 +0200

    Add recipe to database
    Recipe can now be added to database with test class

--- yummy_tummy/lib/model/test.dart
+++ yummy_tummy/lib/model/test.dart
@@ -1,5 +1,9 @@
+import 'dart:collection';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:yummytummy/model/recipe.dart';
 import 'package:yummytummy/model/user.dart';
+import 'package:yummytummy/utils/storeData.dart';
 
 class Test{
   final db;
@@ -9,14 +13,22 @@ class Test{
 
   /// Define test methods here.
   void testMethods() async {
-    testAddUser("tempUser", 0, RankType.beginner);
-    print("Creating user document...");
+    // Get random recipe
+    List<Recipe> recipes = getRecipes();
+
+    // Add a new user to database. Uncomment to test.
+    //testAddUser("tempUser", 0, RankType.beginner);
+    //print("Creating user document...");
+
+    // Add a new recipe to database. Uncomment to test.
+    testAddRecipe(recipes[1]);
+    print("Creating new recipe...");
   }
 
   /// Add a new user to Firestore database.
-  Future<String> testAddUser(String name, int score, RankType type){
+  Future<void> testAddUser(String name, int score, RankType type){
 
-    // Print new document ID to console.
+    // Print new user document ID to console.
     this.db.collection("users").add(
     {
       "name" : name,
@@ -24,6 +36,25 @@ class Test{
       "score" : score,
     }).then((value){
       print("Created new user with ID " + value.documentID);
+    });
+  }
+
+  Future<void> testAddRecipe(Recipe recipe){
+
+    /// Make map with recipe information.
+    Map recipeMap = new HashMap<String, Object>();
+    recipeMap.putIfAbsent("title", () => recipe.getTitle());
+    recipeMap.putIfAbsent("description", () => recipe.getDescription());
+    recipeMap.putIfAbsent("type", () => recipe.getType().index);
+    recipeMap.putIfAbsent("isVegetarian", () => recipe.getIsVegetarian());
+    recipeMap.putIfAbsent("title", () => recipe.getTitle);
+    recipeMap.putIfAbsent("duration", () => recipe.getDuration());
+    recipeMap.putIfAbsent("ingredients", () => recipe.getIngredients());
+    recipeMap.putIfAbsent("imageURL", () => recipe.getImageURL());
+
+    // Print new recipe document ID to console.
+    this.db.collection("recipes").add(recipeMap).then((value){
+      print("Created new recipe with ID " + value.documentID);
     });
   }
 }
