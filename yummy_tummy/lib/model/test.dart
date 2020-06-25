9059beec4221ea7ebf2aa9d8e06d407e8b764de0
Michiel Proost <michiel.proost@gmail.com>
2020-06-25 12:08:00 +0200

    Get All Vegetarian Recipes
    Returning a list of recipes also works now!

--- yummy_tummy/lib/model/test.dart
+++ yummy_tummy/lib/model/test.dart
@@ -5,7 +5,10 @@ import 'package:yummytummy/model/recipe.dart';
 import 'package:yummytummy/model/user.dart';
 import 'package:yummytummy/utils/storeData.dart';
 
-class Test{
+// There is a fetch limit when querying from Firestore.
+const int FETCH_LIMIT = 20;
+
+class Test {
   final db;
 
   /// Test constructor.
@@ -25,53 +28,81 @@ class Test{
     //print("Creating new recipe...");
 
     // Get recipe object from title.
-    Recipe fetchedRecipe = await getRecipeFromTitle("Carpaccio van komkommer met geitenkaas");
+    //Recipe fetchedRecipe = await getRecipeFromTitle("Carpaccio van komkommer met geitenkaas");
     // Print a summary of fetched recipe.
-    fetchedRecipe.printSummary();
+    //fetchedRecipe.printSummary();
+
+    // Get vegetarian recipes from Firestore.
+    List<Recipe> vegetarianRecipes = await getVegetarianRecipes();
+    // Print summary of all vegetarian recipes.
+    for (int i = 0; i < vegetarianRecipes.length; i++){
+      vegetarianRecipes[i].printSummary();
+    }
   }
 
   /// Add a new user to Firestore database.
-  Future<void> testAddUser(String name, int score, RankType type){
-
+  Future<void> testAddUser(String name, int score, RankType type) {
     // Print new user document ID to console.
     this.db.collection("users").add(
-    {
-      "name" : name,
-      "rank" : type.index,
-      "score" : score,
-    }).then((value){
+        {
+          "name": name,
+          "rank": type.index,
+          "score": score,
+        }).then((value) {
       print("Created new user with ID " + value.documentID);
     });
   }
 
   /// Add a new recipe to Firestore database.
-  Future<void> testAddRecipe(Recipe recipe){
-
+  Future<void> testAddRecipe(Recipe recipe) {
     // Make map with recipe information.
     Map recipeMap = new HashMap<String, Object>();
     recipeMap.putIfAbsent("title", () => recipe.getTitle());
     recipeMap.putIfAbsent("description", () => recipe.getDescription());
-    recipeMap.putIfAbsent("type", () => recipe.getType().index);
+    recipeMap.putIfAbsent("type", () =>
+    recipe
+        .getType()
+        .index);
     recipeMap.putIfAbsent("isVegetarian", () => recipe.getIsVegetarian());
     recipeMap.putIfAbsent("duration", () => recipe.getDuration());
     recipeMap.putIfAbsent("ingredients", () => recipe.getIngredients());
     recipeMap.putIfAbsent("imageURL", () => recipe.getImageURL());
 
     // Print new recipe document ID to console.
-    this.db.collection("recipes").add(recipeMap).then((value){
+    this.db.collection("recipes").add(recipeMap).then((value) {
       print("Created new recipe with ID " + value.documentID);
     });
   }
 
-  // Return recipe object when supplying it with a title.
-  Future<Recipe> getRecipeFromTitle(String title) async{
+  /// Return recipe object when supplying it with a title.
+  Future<Recipe> getRecipeFromTitle(String title) async {
     Recipe fetchedRecipe =
     await this.db.collection("recipes")
         .where("title", isEqualTo: title)
         .getDocuments()
-        .then((QuerySnapshot docs){
-        return docs.documents.isNotEmpty ? Recipe.fromMap(docs.documents[0].data, docs.documents[0].documentID) : null;
-        });
+        .then((QuerySnapshot docs) {
+      return docs.documents.isNotEmpty ? Recipe.fromMap(
+          docs.documents[0].data, docs.documents[0].documentID) : null;
+      });
     return fetchedRecipe;
+  }
+
+  /// Last test method. Get all vegetarian recipes from Firestore.
+  Future<List<Recipe>> getVegetarianRecipes() async {
+    List<Recipe> fetchedRecipes=
+    await this.db.collection("recipes")
+        .where("isVegetarian", isEqualTo: true)
+        .getDocuments()
+        //.limit(FETCH_LIMIT)
+        .then((QuerySnapshot docs){
+          Recipe recipe;
+          List<Recipe> recipes = new List(docs.documents.length);
+          for (int i = 0; i < docs.documents.length; i++){
+            recipe = Recipe.fromMap(docs.documents[i].data, docs.documents[i].documentID);
+            recipes[i] = recipe;
+          }
+      return docs.documents.isNotEmpty ? recipes : null;
+    });
+    return fetchedRecipes;
   }
 }
