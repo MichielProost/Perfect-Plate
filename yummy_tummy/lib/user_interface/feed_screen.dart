765fc77a5c244a7d33e1a49c2af225fb1bccff71
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-07-02 19:28:12 +0200

    Show popup on error in feed screen

--- yummy_tummy/lib/user_interface/feed_screen.dart
+++ yummy_tummy/lib/user_interface/feed_screen.dart
@@ -6,13 +6,14 @@ import 'package:yummytummy/database/interfaces/recipeService.dart';
 import 'package:yummytummy/model/recipe.dart';
 import 'package:yummytummy/model/user.dart';
 import 'package:yummytummy/user_interface/components/waiting_indicator.dart';
+import 'package:yummytummy/user_interface/popup/snackbar_util.dart';
 
 import 'components/recipe_card.dart';
 import 'constants.dart';
 
 class FeedScreen extends StatelessWidget {
 
-  final RecipeService _recipeService = DummyDatabase(delayInMilliseconds: 1000);
+  final RecipeService _recipeService = DummyDatabase(delayInMilliseconds: 250);
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +24,12 @@ class FeedScreen extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) 
           {
-            return ListView(
-              children: <Widget>[
-                for (Recipe recipe in snapshot.data) RecipeCard(recipe, showBookmark: false),
+            return snapshot.hasError ?
+              //TODO implement language
+              SnackBarUtil.createTextSnackBar("An error occured while retrieving the recipes!") :
+              ListView(
+                children: <Widget>[
+                  for (Recipe recipe in snapshot.data) RecipeCard(recipe, showBookmark: false),
               ],
             );
           } 
