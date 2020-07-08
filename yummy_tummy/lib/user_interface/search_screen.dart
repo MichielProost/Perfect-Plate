0e035e7dccbe4662ef85742ba6cd8b2b1379074c
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-07-08 17:16:14 +0200

    Search QoL improvements

--- yummy_tummy/lib/user_interface/search_screen.dart
+++ yummy_tummy/lib/user_interface/search_screen.dart
@@ -59,132 +59,148 @@ class _SearchScreen extends State<SearchScreen> {
     );
   }
 
+  var _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          // TODO implement language system
-          // Headline
-          Center(
-            child: Text(
-              "Search for recipes",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-          ),
 
           // Searchbox
-          Card(
-            child: Column(
-              children: <Widget>[
-                GridView.count(
-                  shrinkWrap: true,
-                  childAspectRatio: 4.0 / 1.0,
-                  crossAxisCount: 2,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                child: Column(
                   children: <Widget>[
-                    
-                    Padding(
-                      padding: const EdgeInsets.only(left: 60.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text("Diet of choice")
-                      ),
-                    ),
 
-                    DropdownButton<DietField>(
-                      value: _dietField,
-                      icon: Icon(Icons.keyboard_arrow_down),
-                      isExpanded: true,
-                      iconSize: 24,
-                      elevation: 16,
-                      style: TextStyle(color: Colors.black),
-                      onChanged: (DietField newValue) {
-                        setState(() {
-                          _dietField = newValue;
-                        });
-                      },
-                      items: DietField.values
-                          .map<DropdownMenuItem<DietField>>((DietField value) {
-                        return DropdownMenuItem<DietField>(
-                          value: value,
-                          child: Text(value.getString()),
-                        );
-                      }).toList(),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(left: 60.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text("Type of dish")
-                      ),
-                    ),
-
-                    DropdownButton<RecipeType>(
-                      value: _recipeType,
-                      icon: Icon(Icons.keyboard_arrow_down),
-                      isExpanded: true,
-                      iconSize: 24,
-                      elevation: 16,
-                      style: TextStyle(color: Colors.black),
-                      onChanged: (RecipeType newValue) {
-                        setState(() {
-                          _recipeType = newValue;
-                        });
-                      },
-                      items: RecipeType.values
-                          .map<DropdownMenuItem<RecipeType>>((RecipeType value) {
-                        return DropdownMenuItem<RecipeType>(
-                          value: value,
-                          child: Text(value.getString()),
-                        );
-                      }).toList(),
-                    ),
-
-                  ],
-                ),
-                
-                SizedBox(
-                  height: 30.0,
-                ),
-                
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                  child: Column(
-                    children: <Widget>[
-                      Text("Add required ingredients below:"),
-                      TextFormField(
-                        enableSuggestions: true,
-                        maxLines: 1,
-                        onFieldSubmitted: (String text) {
-                          setState(() {
-                            if (text != "")
-                              _ingredients.add( text );
-                          });
-                        },
-                        decoration: InputDecoration(
-                          focusColor: Constants.main,
-                          prefix: Text("Add required ingredients here"),
+                    // Headline
+                    Center(
+                      child: Text(
+                        "Search for recipes",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
                         ),
                       ),
-                    ],
-                  ),
-                ),
+                    ),
 
-                ListView(
-                  shrinkWrap: true,
-                  children: <Widget>[
-                    // Display selected recipes
-                    for (int i = 0; i < _ingredients.length; i++)
-                      buildIngredientDisplay(i)
+                    GridView.count(
+                      shrinkWrap: true,
+                      childAspectRatio: 4.0 / 1.0,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        
+                        Padding(
+                          padding: const EdgeInsets.only(left: 60.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Diet of choice")
+                          ),
+                        ),
+
+                        DropdownButton<DietField>(
+                          value: _dietField,
+                          icon: Icon(Icons.keyboard_arrow_down),
+                          isExpanded: true,
+                          iconSize: 24,
+                          elevation: 16,
+                          style: TextStyle(color: Colors.black),
+                          onChanged: (DietField newValue) {
+                            setState(() {
+                              _dietField = newValue;
+                            });
+                          },
+                          items: DietField.values
+                              .map<DropdownMenuItem<DietField>>((DietField value) {
+                            return DropdownMenuItem<DietField>(
+                              value: value,
+                              child: Text(value.getString()),
+                            );
+                          }).toList(),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(left: 60.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("Type of dish")
+                          ),
+                        ),
+
+                        DropdownButton<RecipeType>(
+                          value: _recipeType,
+                          icon: Icon(Icons.keyboard_arrow_down),
+                          isExpanded: true,
+                          iconSize: 24,
+                          elevation: 16,
+                          style: TextStyle(color: Colors.black),
+                          onChanged: (RecipeType newValue) {
+                            setState(() {
+                              _recipeType = newValue;
+                            });
+                          },
+                          items: RecipeType.values
+                              .map<DropdownMenuItem<RecipeType>>((RecipeType value) {
+                            return DropdownMenuItem<RecipeType>(
+                              value: value,
+                              child: Text(value.getString()),
+                            );
+                          }).toList(),
+                        ),
+
+                      ],
+                    ),
+                    
+                    SizedBox(
+                      height: 30.0,
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text("Add required ingredients below:"),
+                          TextFormField(
+                            enableSuggestions: true,
+                            controller: _controller,
+                            maxLines: 1,
+                            onFieldSubmitted: (String text) {
+                              setState(() {
+                                if (text != "") {
+                                  _ingredients.add( text );
+                                  _controller.clear();
+                                }
+                              });
+                            },
+                            decoration: InputDecoration(
+                              focusColor: Constants.main,
+                              hintText: "Add required ingredients here",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 30.0,
+                    ),
+
+                    ListView(
+                      shrinkWrap: true,
+                      children: <Widget>[
+                        // Display selected recipes
+                        for (int i = 0; i < _ingredients.length; i++)
+                          buildIngredientDisplay(i)
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
