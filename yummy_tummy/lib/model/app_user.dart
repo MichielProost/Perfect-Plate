2385348c78e20699ba92ec085d80e4043723f4b8
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-07-13 18:08:18 +0200

    Add app user

---
+++ yummy_tummy/lib/model/app_user.dart
@@ -0,0 +1,18 @@
+import 'package:yummytummy/model/user.dart';
+
+/// A class that contains info on a logged in user
+class AppUser extends User {
+
+  final String _googleID;
+
+  AppUser(this._googleID, Map<String, dynamic> userData) : super.fromMap( userData, _googleID );
+
+  AppUser.offline() : _googleID = "", super(id: "", name: "User", rank: RankType.values[0], score: 0, favourites: List<String>());
+
+  /// Check if this user is logged in
+  bool isLoggedIn()
+  {
+    return _googleID != "";
+  }
+
+}
