2423e26ffa842127ad6edea9b6a5a11b67d12b2f
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-07-19 21:29:15 +0200

    Implement actual feed (temp based on weighted rating)

--- yummy_tummy/lib/user_interface/feed_screen.dart
+++ yummy_tummy/lib/user_interface/feed_screen.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:yummytummy/database/dummy/dummydatabase.dart';
+import 'package:yummytummy/database/firestore/recipeServiceFirestore.dart';
 import 'package:yummytummy/database/interfaces/recipeService.dart';
+import 'package:yummytummy/database/query/queryInfo.dart';
 import 'package:yummytummy/model/recipe.dart';
 import 'package:yummytummy/model/user.dart';
 import 'package:yummytummy/user_interface/components/waiting_indicator.dart';
@@ -13,15 +15,18 @@ import 'constants.dart';
 
 class FeedScreen extends StatelessWidget {
 
-  final RecipeService _recipeService = DummyDatabase(delayInMilliseconds: 250);
+  final RecipeService _recipeService = RecipeServiceFirestore();
 
   @override
   Widget build(BuildContext context) {
+
+    RecipeQuery queryInfo = RecipeQuery();
+
     return Scaffold(
       backgroundColor: Constants.background,
-      body: FutureBuilder(
+      body: FutureBuilder<RecipeQuery> (
         //TODO replace by feed algorithm
-        future: _recipeService.getRecipesFromUser(UserMapField.id, 'id'),
+        future: _recipeService.searchRecipes(queryInfo, SortField.weightedRating, DietField.any, RecipeType.any, List<String>()),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) 
           {
@@ -32,7 +37,22 @@ class FeedScreen extends StatelessWidget {
                 data: Constants.themeData,
                 child: ListView(
                   children: <Widget>[
-                    for (Recipe recipe in snapshot.data) RecipeCard(recipe, showBookmark: false),
+                    
+                    if (snapshot.data.recipes.length == 0)
+                      Padding(
+                        padding: const EdgeInsets.only( top: 20.0 ),
+                        child: Text(
+                          "Woops, no recipes found! :-(",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                    for (Recipe recipe in snapshot.data.recipes) 
+                      RecipeCard(recipe, showBookmark: false),
 
                     SizedBox(
                       height: 30.0,
