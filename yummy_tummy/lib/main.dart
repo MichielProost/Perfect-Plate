5728da7a2b846250a5dcf76418bfec20d1590642
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-07-25 11:22:57 +0200

    Auto log in on startup

--- yummy_tummy/lib/main.dart
+++ yummy_tummy/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yummytummy/database/authentication/google.dart';
 import 'package:yummytummy/user_interface/screen_handler.dart';
 import 'model/test.dart';
 
@@ -13,6 +14,9 @@ class YummyTummy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    GoogleAuthHandler().handleSignIn();
+    
     return MaterialApp(
       title: "Yummy Tummy",
       // TODO implement home screen
