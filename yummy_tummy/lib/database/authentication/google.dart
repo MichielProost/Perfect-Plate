006bdeb00bb2965f03442e08fc162429dda3503e
Michiel Proost <michiel.proost@gmail.com>
2020-08-09 15:53:40 +0200

    Fixed Log-in Error

--- yummy_tummy/lib/database/authentication/google.dart
+++ yummy_tummy/lib/database/authentication/google.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:yummytummy/database/firestore/userServiceFirestore.dart';
@@ -22,55 +21,63 @@ class GoogleAuthHandler{
   /// Returns true if the user already exists
   /// Returns false if this is a new user
   Future<bool> handleSignIn({HomeScreenState screen}) async {
-    
+
+    bool userExists = false;
+
     // Stop homescreen touching
     if (screen != null)
       screen.setTouchable(false);
 
-    // TODO Michiel: handle log in cancel (PlatformException!!)
-    GoogleSignInAccount googleAccount = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleAuth = await googleAccount.authentication;
+    GoogleSignInAccount googleAccount =
+      await _googleSignIn.signIn().catchError((onError){
+        print("User cancelled Sign in. Error: $onError");
+      });
 
-    AuthCredential credential = GoogleAuthProvider.getCredential(
-        idToken: googleAuth.idToken,
-        accessToken: googleAuth.accessToken
-    );
-    
-    AuthResult result = (await _auth.signInWithCredential(credential));
+    if (googleAccount != null)
+    {
+      GoogleSignInAuthentication googleAuth = await googleAccount.authentication;
 
-    FirebaseUser googleUser = result.user;
-
-    Map<String, dynamic> userData;
-
-    // Get user data from database.
-    bool userExists = await userService.userExists(googleUser.uid);
-    if (userExists){
-      // Get existing user.
-      User user = await userService.getUserFromID(googleUser.uid);
-      userData = user.toMap();
-    } else {
-      // Create a new user.
-      User user = new User(
-        id: googleUser.uid,
-        name: googleUser.displayName,
-        score: 0,
-        rank: RankType.dishwasher,
-        dietFieldPreference: DietField.any,
-        recipeTypePreference: RecipeType.any,
-        languagePreference: LanguagePick.english,
-        favourites: [],
-        image: '',
-        board: new MedalBoard(
-            seriesMap: dataToSeriesMap(getDefaultDataMap()))
+      AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken
       );
-      userData = user.toMap();
-      // Create new user document.
-      await userService.addUser(user, googleUser.uid);
+
+      AuthResult result = (await _auth.signInWithCredential(credential));
+
+      FirebaseUser googleUser = result.user;
+
+      Map<String, dynamic> userData;
+
+      // Get user data from database.
+      userExists = await userService.userExists(googleUser.uid);
+      if (userExists){
+        // Get existing user.
+        User user = await userService.getUserFromID(googleUser.uid);
+        userData = user.toMap();
+      } else {
+        // Create a new user.
+        User user = new User(
+            id: googleUser.uid,
+            name: googleUser.displayName,
+            score: 0,
+            rank: RankType.dishwasher,
+            dietFieldPreference: DietField.any,
+            recipeTypePreference: RecipeType.any,
+            languagePreference: LanguagePick.english,
+            favourites: [],
+            image: '',
+            board: new MedalBoard(
+                seriesMap: dataToSeriesMap(getDefaultDataMap()))
+        );
+        userData = user.toMap();
+        // Create new user document.
+        await userService.addUser(user, googleUser.uid);
+      }
+
+      Constants.appUser = new AppUser(googleUser.uid, userData);
+
+      userService.scoreListener();
     }
-
-    Constants.appUser = new AppUser(googleUser.uid, userData);
-
-    userService.scoreListener();
 
     // Re-enable homescreen touching
     if (screen != null)
